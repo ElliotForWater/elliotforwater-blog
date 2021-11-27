20bdd46ff23c4944c8971a5e0324a66cec1832c0
designbygio <gsambrotta@users.noreply.github.com>
2021-11-27 13:34:25 +0100

    add ga on pageview (#3)
    * add also github templates

---
+++ .github/pull_request_template.md
@@ -0,0 +1,29 @@
+### Related Issue
+Link here the issue related to this PR, if any,
+
+## Description
+A few sentences describing the overall goals of the pull request's commits.
+Please follow a Before/After kind of approach for explaining your changes and attach screenshots if any.
+
+
+#### Impacted Areas in Application
+List general components of the application that this PR will affect:
+
+
+#### Steps to Test or Reproduce
+Outline the steps to test or reproduce the PR here.
+
+
+#### Related PRs
+If you PR is related to other part of PR, list them here
+
+------------------------------------------------
+## PR Checklist:
+
+- [ ] My code follows the style guidelines of this project and I have double check my own code
+- [ ] I have made corresponding changes to the documentation, if any
+- [ ] My changes generate no new warnings
+- [ ] I have run `npm run build:stage` and be sure no error block the build
+- [ ] I have test locally that everything run as expected
+- [ ] I have properly fill in the PR template
+- [ ] I have ask for reviews
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,41 @@
-import React from 'react';
+import React from 'react'
 
-import Document, {
-  Html, Head, Main, NextScript,
-} from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+import { GA_TRACKING_ID } from '../helpers/_gtag'
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
-        <Head />
+      <Html lang='en'>
+        <Head>
+          {process.env.NEXT_PUBLIC_IS_PRODUCTION && (
+            <>
+              {/* Global Site Tag (gtag.js) - Google Analytics */}
+              <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '${GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                }}
+              />
+            </>
+          )}
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
