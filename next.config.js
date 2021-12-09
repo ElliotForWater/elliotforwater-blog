1af482aa48bf497f10137c0a2587bff9e6095d25
designbygio <designbygio@pm.me>
2021-12-09 21:09:32 +0100

    remove rewrites

--- next.config.js
+++ next.config.js
@@ -10,20 +10,4 @@ const baseUrl = '/blog'
 module.exports = withPlugins([[withBundleAnalyzer], nextTranslate], {
   basePath: baseUrl,
   assetPrefix: baseUrl,
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'it', 'es'],
-  },
-  async rewrites() {
-    return [
-      // {
-      //   source: '/blog',
-      //   destination: `${process.env.NEXT_PUBLIC_BLOG_URL}/blog`,
-      // },
-      {
-        source: '/blog/it/:path*',
-        destination: `${process.env.NEXT_PUBLIC_BLOG_URL}/it/blog/:path*`,
-      },
-    ]
-  },
 })
